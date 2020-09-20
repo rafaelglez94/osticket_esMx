249a842f763d65006beef34cf8f7ad92cfb0ec51
Rafael Gonzalez <rafa.gc2807@gmail.com>
2020-09-20 12:22:07 -0600

    esperemos que funcione

---
+++ es_MX/MANIFEST.php
@@ -0,0 +1,10 @@
+<?php return array (
+  'Build-Date' => 'Wed, 06 Feb 19 21:20:44 +0000',
+  'Phrases-Version' => '1.10',
+  'Build-Version' => 'v1.11',
+  'Build-Major-Version' => '1.10',
+  'Language' => 'es_MX',
+  'Id' => 'lang:es_MX',
+  'Last-Revision' => '2019-02-06 21:42',
+  'Version' => 154948,
+);
